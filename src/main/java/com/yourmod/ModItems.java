1ad2dbe7afd47851ae522a4434399f8b1e0125b9
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-07-18 08:54:59 +0900

    Update ModItems.java

--- src/main/java/com/yourmod/ModItems.java
+++ src/main/java/com/yourmod/ModItems.java
@@ -1,6 +1,9 @@
 package com.yourmod;
 
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -13,9 +16,19 @@ public class ModItems {
         copperIngot = new ItemCopperIngot();
     }
 
+    // 物品注册事件 (保持不变)
     @SubscribeEvent
     public static void registerItems(RegistryEvent.Register<Item> event) {
         event.getRegistry().register(copperIngot);
-        System.out.println("铜锭已注册！"); // 控制台输出，方便调试
+    }
+
+
+    @SubscribeEvent
+    public static void registerModels(ModelRegistryEvent event) {
+        ModelLoader.setCustomModelResourceLocation(
+            copperIngot, 
+            0, 
+            new ModelResourceLocation(copperIngot.getRegistryName(), "inventory")
+        );
     }
 }
