5dc69c7472b516023d744fac4eed9c4ed67028c1
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-07-20 10:07:58 +0900

    Update ModItems.java

--- src/main/java/com/yourmod/ModItems.java
+++ src/main/java/com/yourmod/ModItems.java
@@ -1,6 +1,7 @@
 package com.yourmod;
 
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.client.model.ModelLoader;
@@ -8,27 +9,43 @@ import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Mod.EventBusSubscriber(modid = YourMod.MODID)
 public class ModItems {
-    public static Item copperIngot;
+    public static final List<DynamicItem> ITEMS = new ArrayList<>();
 
+    // 从配置文件加载并创建物品
     public static void init() {
-        copperIngot = new ItemCopperIngot();
+        ITEMS.clear();
+        
+        List<ConfigLoader.ItemData> dataList = ConfigLoader.loadItemsProperly();
+        
+        for (ConfigLoader.ItemData data : dataList) {
+            DynamicItem item = new DynamicItem(data.id, data.quantity, data.creativeTab);
+            ITEMS.add(item);
+            System.out.println("[YourMod] 已创建物品: " + data.id);
+        }
     }
 
-    // 物品注册事件 (保持不变)
     @SubscribeEvent
     public static void registerItems(RegistryEvent.Register<Item> event) {
-        event.getRegistry().register(copperIngot);
+        for (DynamicItem item : ITEMS) {
+            event.getRegistry().register(item);
+            System.out.println("[YourMod] 已注册物品: " + item.getItemId());
+        }
     }
-
 
     @SubscribeEvent
     public static void registerModels(ModelRegistryEvent event) {
-        ModelLoader.setCustomModelResourceLocation(
-            copperIngot, 
-            0, 
-            new ModelResourceLocation(copperIngot.getRegistryName(), "inventory")
-        );
+        for (DynamicItem item : ITEMS) {
+            ModelLoader.setCustomModelResourceLocation(
+                item,
+                0,
+                new ModelResourceLocation(item.getRegistryName(), "inventory")
+            );
+            System.out.println("[YourMod] 已注册模型: " + item.getItemId());
+        }
     }
 }
