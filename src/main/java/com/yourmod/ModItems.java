de89c03c1f6cd4589f47a0921085cf19234e8073
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-07-20 15:42:10 +0900

    Update ModItems.java

--- src/main/java/com/yourmod/ModItems.java
+++ src/main/java/com/yourmod/ModItems.java
@@ -1,7 +1,6 @@
 package com.yourmod;
 
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
-import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.client.model.ModelLoader;
@@ -14,38 +13,30 @@ import java.util.List;
 
 @Mod.EventBusSubscriber(modid = YourMod.MODID)
 public class ModItems {
-    public static final List<DynamicItem> ITEMS = new ArrayList<>();
+    public static final List<Item> ITEMS = new ArrayList<>();
 
-    // 从配置文件加载并创建物品
     public static void init() {
         ITEMS.clear();
-        
-        List<ConfigLoader.ItemData> dataList = ConfigLoader.loadItems();
-        
-        for (ConfigLoader.ItemData data : dataList) {
-            DynamicItem item = new DynamicItem(data.id, data.quantity, data.creativeTab);
-            ITEMS.add(item);
-            System.out.println("[YourMod] 已创建物品: " + data.id);
+        for (ConfigLoader.ItemData data : ConfigLoader.loadItems()) {
+            ITEMS.add(new DynamicItem(data));
         }
     }
 
     @SubscribeEvent
     public static void registerItems(RegistryEvent.Register<Item> event) {
-        for (DynamicItem item : ITEMS) {
+        for (Item item : ITEMS) {
             event.getRegistry().register(item);
-            System.out.println("[YourMod] 已注册物品: " + item.getItemId());
         }
     }
 
     @SubscribeEvent
     public static void registerModels(ModelRegistryEvent event) {
-        for (DynamicItem item : ITEMS) {
+        for (Item item : ITEMS) {
             ModelLoader.setCustomModelResourceLocation(
                 item,
                 0,
                 new ModelResourceLocation(item.getRegistryName(), "inventory")
             );
-            System.out.println("[YourMod] 已注册模型: " + item.getItemId());
         }
     }
 }
